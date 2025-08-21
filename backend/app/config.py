8fddd21bee99154755ca6f4d58da2e27c1c5dbd3
Sawyer Jones <sjones3731@sdsu.edu>
2025-08-21 09:13:59 -0700

    created backend structure

---
+++ backend/app/config.py
@@ -0,0 +1,13 @@
+from decouple import config
+import os
+
+SECRET_KEY = config("SECRET_KEY")
+DEBUG = config("DEBUG", cast="bool")
+
+STORAGE_PATH = config("STORAGE_PATH")
+MAX_FILE_SIZE = config("MAX_FILE_SIZE")
+
+API_V1_PREFIX = "/api/v1"
+CORS_ORIGINS = config("CORS_ORIGINS")
+
+os.makedirs(STORAGE_PATH, exist_ok=True)
